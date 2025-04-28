86fce4e37715ae5eb87e164bcb506c6f08877ab2
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-04-29 01:10:49 +0800

    added question dialog

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -13,12 +13,14 @@ import NewQuestionDialog from "@/components/NewQuestionDialog"
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
-      <section className="py-4">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Analytic dApp</h4>
+      <div className="py-4">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Analytics dApp</h4>
         <p className="text-muted-foreground text-center">built on Zama</p>
-      </section>
+      </div>
 
-      <NewQuestionDialog/>
+      <div>
+        <NewQuestionDialog/>
+      </div>
 
       <div className="self-start px-6">
         <Card className="w-[350px]">
--- packages/web/src/components/NewQuestionDialog.tsx
+++ packages/web/src/components/NewQuestionDialog.tsx
@@ -1,33 +1,52 @@
 'use client';
-
+import { useState } from 'react'
+import { Description, Dialog, DialogPanel, DialogTitle, DialogBackdrop, Field, Label, Input } from '@headlessui/react'
 import { MessageCircleQuestion } from "lucide-react"
+import clsx from "clsx";
 import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 
 export default function NewQuestionDialog() {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" size="lg">
-          <MessageCircleQuestion/>
-          New Question
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create a New Question</DialogTitle>
-        </DialogHeader>
-        <DialogFooter>
-          <Button>Create Question</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+  let [isDialogOpen, setDialogOpen] = useState<boolean>(false)
+
+  const labelClasses="text-sm/6 font-medium text-black";
+  const textInputClasses=clsx(
+    "mt-3 block w-full rounded-lg border-none bg-black/5 px-3 py-1.5 text-sm/6 text-black col-span-3",
+    "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-black/25"
   )
+
+  return <>
+    <Button variant="outline" size="lg" onClick={() => setDialogOpen(true)}>
+      <MessageCircleQuestion/>
+      New Question
+    </Button>
+    <Dialog transition open={isDialogOpen} onClose={() => setDialogOpen(false)}
+      className="relative z-50 transition duration-300 ease-out data-closed:opacity-0"
+    >
+      <DialogBackdrop className="fixed inset-0 bg-black/30" />
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel className="max-w-lg space-y-4 border bg-white p-12 rounded-lg shadow-xl">
+          <DialogTitle className="font-bold">New Question</DialogTitle>
+
+          <form>
+            <Field>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label className={labelClasses}>Text</Label>
+                <Input id="qText" name="qText" className={textInputClasses} />
+              </div>
+            </Field>
+
+            <Field>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label className={labelClasses}>Type</Label>
+              </div>
+            </Field>
+          </form>
+
+          <div className="flex gap-4 items-center justify-center">
+            <Button>Create Question</Button>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  </>
 }
