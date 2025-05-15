89bc285854e731c6c3ba6d7cfc9a6d95392a7bc4
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-05-15 11:18:35 +0800

    updated with docs

--- packages/hardhat/contracts/QuestionSpecLib.sol
+++ packages/hardhat/contracts/QuestionSpecLib.sol
@@ -3,26 +3,51 @@ pragma solidity ^0.8.24;
 
 uint16 constant TXT_MAX_LEN = 512;
 
+/**
+ * @title QuestionSpecLib
+ * @dev This library defines the data structure of a Question in a Question set. It also provide
+ *   a "method" to validate itself.
+ */
 library QuestionSpecLib {
     enum QuestionType {
         Option,
         Value
     }
 
+    /// @title QuestionSpec
     struct QuestionSpec {
+        /// @dev Question text
         string text;
+        /// @dev A list of option texts
         string[] options;
+        /// @dev The min value for this question
         uint32 min;
+        /// @dev The max value for this question
         uint32 max;
+        /// @dev Question type
         QuestionType t;
     }
 
     error InvalidQuestionSpecParam(string reason);
 
+    /// @dev If the question type is `QuestionType.Option`, the object must contains a list of string
+    ///   as options, and `min` be set to 0 and `max` to option length - 1.
+    ///   If question type is `QuestionType.Value`, options must be empty.
+    /// @param self The QuestionSpec (or Question) object
     function validate(QuestionSpec memory self) public pure {
         if (bytes(self.text).length > TXT_MAX_LEN) revert InvalidQuestionSpecParam("questionText max length exceeded");
-        if (self.t == QuestionType.Option && self.options.length < 2)
-            revert InvalidQuestionSpecParam("Options should be greater than 1");
-        if (self.min >= self.max) revert InvalidQuestionSpecParam("min should be less than max");
+
+        if (self.t == QuestionType.Option) {
+            // QuestionType.Option type
+            if (self.options.length < 2) revert InvalidQuestionSpecParam("Options should be greater than 1");
+            if (self.min != 0) revert InvalidQuestionSpecParam("min must be 0 for Option type question");
+            if (self.max != self.options.length - 1)
+                revert InvalidQuestionSpecParam("max must be option length - 1 for Option type question");
+        } else {
+            // QuestionType.Value type
+            if (self.options.length > 0)
+                revert InvalidQuestionSpecParam("Options should be empty for Value type question");
+            if (self.min > self.max) revert InvalidQuestionSpecParam("min should be less than or equal to max");
+        }
     }
 }
--- packages/hardhat/contracts/interfaces/IAnalytic.sol
+++ packages/hardhat/contracts/interfaces/IAnalytic.sol
@@ -12,9 +12,13 @@ interface IAnalytic {
     }
 
     enum PredicateOp {
+        /// @dev equal
         EQ,
+        /// @dev not equal
         NE,
+        /// @dev greater than or equal
         GE,
+        /// @dev less than or equal
         LE
     }
 
@@ -29,39 +33,64 @@ interface IAnalytic {
         Closed
     }
 
+    /// This object type is also called as Quesetion Set in documentation
     struct Question {
+        /// @dev The main question
         QuestionSpecLib.QuestionSpec main;
+        /// @dev A list of meta questions
         QuestionSpecLib.QuestionSpec[] metas;
+        /// @dev time the question starts accepting answers
         uint256 startTime;
+        /// @dev time the question will no longer accepting answers
         uint256 endTime;
         QuestionState state;
+        /// @dev The minimum number of answers before a query request can issue to the question.
         uint32 queryThreshold;
     }
 
+    /// Query Request object type
     struct QueryRequest {
+        /// @dev The question ID this query request refers to
         uint64 questionId;
+        /// @dev Wwner of the query request
         address owner;
+        /// @dev a list of predicates of this query request.
         Predicate[] predicates;
+        /// @dev The accumulated intermediate result so far in the query request
         euint32[] acc;
+        /// @dev Number of answers that satisfy the predicates and got accumulated.
         euint32 ansCount;
+        /// @dev Total number of answers the query request has processed.
         uint32 accSteps;
+        /// @dev Query request state
         RequestState state;
     }
 
+    /// Query Result object type
     struct QueryResult {
+        /// @dev The accumulated result of the query request
         euint32[] acc;
+        /// @dev Number of answers that satisfy the predicates and got accumulated.
         euint32 filteredAnsCount;
+        /// @dev Total number of answers the query request has processed.
         uint32 ttlAnsCount;
     }
 
+    /// Predicate object type
     struct Predicate {
+        /// @dev The index of the meta questions this predicate refer to.
         uint8 metaOpt;
+        /// @dev Predicate operator
         PredicateOp op;
+        /// @dev The target value to be compared against
         uint32 metaVal;
     }
 
+    /// User answer object type
     struct Answer {
+        /// @dev Encrypted answer value for the main question in the question set.
         euint32 val;
+        /// @dev Encrypted answer values for a list of meta questions in the question set.
         euint32[] metaVals;
     }
 
